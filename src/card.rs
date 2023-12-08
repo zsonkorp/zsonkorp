59d93cc7f9132665d1524c9a392315926cf2a47c
Zixuan Song <zsong@777part.com>
2023-12-07 17:04:44 -0800

    Card and deck tests

--- src/card.rs
+++ src/card.rs
@@ -1,11 +1,12 @@
-const RANK_COUNT: u8 = 13;
-const SUIT_COUNT: u8 = 4;
+pub(crate) const RANK_COUNT: u8 = 13;
+pub(crate) const SUIT_COUNT: u8 = 4;
 
 pub(crate) struct Card {
     val: u8
 }
 
-enum Suit {
+#[derive(PartialEq, Debug)]
+pub enum Suit {
     HEARTS,
     SPADES,
     CLUBS,
@@ -35,39 +36,145 @@ impl From<Suit> for u8 {
     }
 }
 
-// impl Suit {
-//     fn to_ordinal(&self) -> u8 {
-//         match self {
-//             Suit::HEARTS => 0,
-//             Suit::SPADES => 1,
-//             Suit::CLUBS => 2,
-//             Suit::DIAMONDS => 3
-//         }
-//     }
-//
-//     fn from_ordinal(ordinal: u8) -> Suit {
-//         match ordinal {
-//             0 => Suit::HEARTS,
-//             1 => Suit::SPADES,
-//             2 => Suit::CLUBS,
-//             3 => Suit::DIAMONDS,
-//             _ => panic!("Invalid suit")
-//         }
-//     }
-// }
-
 impl Card {
     pub fn new(rank: u8, suit: Suit) -> Self {
+
+        if rank == 0 || rank > RANK_COUNT {
+            panic!()
+        }
+
         Card {
-            val: suit.into() * RANK_COUNT + rank
+            val: u8::from(suit) * RANK_COUNT + (rank - 1)
         }
     }
 
     pub fn get_rank(&self) -> u8 {
-        self.val % SUIT_COUNT
+        self.val % RANK_COUNT + 1
     }
 
     pub fn get_suit(&self) -> Suit {
-        Suit::from(self.val / SUIT_COUNT)
+        Suit::from(self.val / RANK_COUNT)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    impl Card {
+        pub fn get_val(&self) -> u8 {
+            self.val
+        }
+    }
+
+    #[test]
+    fn suit_to_ord() {
+        assert_eq!(u8::from(Suit::HEARTS), 0);
+        assert_eq!(u8::from(Suit::SPADES), 1);
+        assert_eq!(u8::from(Suit::CLUBS), 2);
+        assert_eq!(u8::from(Suit::DIAMONDS), 3);
+    }
+
+    #[test]
+    fn ord_to_suit() {
+        assert_eq!(Suit::from(0), Suit::HEARTS);
+        assert_eq!(Suit::from(1), Suit::SPADES);
+        assert_eq!(Suit::from(2), Suit::CLUBS);
+        assert_eq!(Suit::from(3), Suit::DIAMONDS);
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_ord_to_suit_edge() {
+        let _ = Suit::from(4);
+    }
+
+    #[test]
+    fn create_card_suit() {
+        let card = Card::new(1, Suit::HEARTS);
+        assert_eq!(card.val, 0);
+
+        let card = Card::new(1, Suit::SPADES);
+        assert_eq!(card.val, RANK_COUNT);
+
+        let card = Card::new(1, Suit::CLUBS);
+        assert_eq!(card.val, RANK_COUNT * 2);
+
+        let card = Card::new(1, Suit::DIAMONDS);
+        assert_eq!(card.val, RANK_COUNT * 3);
+    }
+
+    #[test]
+    fn create_card_rank() {
+        let card = Card::new(1, Suit::HEARTS);
+        assert_eq!(card.val, 0);
+
+        let card = Card::new(2, Suit::HEARTS);
+        assert_eq!(card.val, 1);
+
+        let card = Card::new(6, Suit::HEARTS);
+        assert_eq!(card.val, 5);
+
+        let card = Card::new(7, Suit::HEARTS);
+        assert_eq!(card.val, 6);
+
+        let card = Card::new(13, Suit::HEARTS);
+        assert_eq!(card.val, 12);
+
+        let card = Card::new(12, Suit::HEARTS);
+        assert_eq!(card.val, 11);
+    }
+
+    // #[test]
+    fn create_card_rank_suit_mix() {
+        todo!()
+    }
+
+    #[test]
+    #[should_panic]
+    fn create_card_invalid_rank() {
+        todo!()
+    }
+
+    #[test]
+    fn get_rank() {
+        let card = Card { val: 0 };
+        assert_eq!(card.get_rank(), 1);
+
+        let card = Card { val: 13 };
+        assert_eq!(card.get_rank(), 1);
+
+        let card = Card { val: 5 };
+        assert_eq!(card.get_rank(), 6);
+
+        let card = Card { val: 12 };
+        assert_eq!(card.get_rank(), 13);
+
+        let card = Card { val: 13 };
+        assert_eq!(card.get_rank(), 1);
+
+        let card = Card { val: 13 };
+        assert_eq!(card.get_rank(), 1);
+    }
+
+    #[test]
+    fn get_suit() {
+        let card = Card { val: 0 };
+        assert_eq!(card.get_suit(), Suit::HEARTS);
+
+        let card = Card { val: 13 };
+        assert_eq!(card.get_suit(), Suit::SPADES);
+
+        let card = Card { val: 26 };
+        assert_eq!(card.get_suit(), Suit::CLUBS);
+
+        let card = Card { val: 39 };
+        assert_eq!(card.get_suit(), Suit::DIAMONDS);
+
+        let card = Card { val: 5 };
+        assert_eq!(card.get_suit(), Suit::HEARTS);
+
+        let card = Card { val: 12 };
+        assert_eq!(card.get_suit(), Suit::HEARTS);
     }
 }
--- src/deck.rs
+++ src/deck.rs
@@ -1,4 +1,6 @@
-use crate::card::Card;
+use crate::card::{Card, RANK_COUNT, SUIT_COUNT};
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 struct Deck {
     cards: Vec<Card>
@@ -6,14 +8,67 @@ struct Deck {
 
 impl Deck {
     pub fn new() -> Self {
-        todo!()
+        Deck::new_multi(1)
     }
 
     pub fn new_multi(count: u32) -> Self {
-        todo!()
+        match count {
+            0 => panic!(),   //very nice
+            _ => {
+                let mut cards = Vec::with_capacity(
+                    (count * RANK_COUNT as u32 * SUIT_COUNT as u32) as usize
+                );
+
+                for _ in 0..count {
+                    for suit_ord in 0..SUIT_COUNT {
+                        for rank in 1..(RANK_COUNT + 1) {
+                            cards.push(Card::new(rank, suit_ord.into()));
+                        }
+                    }
+                }
+
+                return Deck { cards };
+            }
+        }
     }
 
-    pub fn shuffle(&self) {
+    pub fn shuffle(&mut self) {
+        self.cards.shuffle(&mut thread_rng());
+    }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_single_deck() {
+        let deck = Deck::new();
+
+        assert_eq!(deck.cards.len(), 52);
+
+        for i in 0..52 {
+            assert_eq!(deck.cards[i].get_val() as usize, i);
+        }
+    }
+
+    #[test]
+    fn create_multi_deck() {
+        let deck_count = 6;
+        let deck = Deck::new_multi(deck_count);
+
+        assert_eq!(deck.cards.len(), 52 * deck_count);
+
+        for i in 0..deck_count {
+            for j in 0..52 {
+                assert_eq!(deck.cards[i * 52 + j].get_val() as usize, j);
+            }
+        }
+    }
+
+    #[test]
+    #[should_panic]
+    fn create_invalid_multi_deck() {
+        todo!()
     }
 }
