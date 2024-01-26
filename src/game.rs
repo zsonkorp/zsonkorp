620fcce295d4814024b6449683394eda7c5af11c
zixuansong <zsong249@gmail.com>
2024-01-26 00:26:18 -0800

    dynamic game creation, some error handling

--- src/game.rs
+++ src/game.rs
@@ -1,20 +1,41 @@
 use thiserror::Error;
-use anyhow::Result;
+use anyhow::{anyhow, Result};
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde::Deserialize;
 
 pub(crate) mod fts;
 pub use fts::Fts;
+use crate::{config, game};
 
 #[derive(Error, Debug)]
-enum Error {
+pub(crate) enum Error {
     #[error("Generic game error")]
-    Generic
+    Generic,
+    #[error("Error parsing game configuration: {0}")]
+    ParseConfig(String),
+    #[error("Unknown game type")]
+    UnknownGame
 }
 
-enum GameType {
+#[derive(Deserialize)]
+pub(crate) enum GameType {
     Fts
 }
+
 pub trait Game: Sync + Send {
     fn my_type(&self) -> GameType;
     fn start(&mut self) -> Result<()>;
     fn get_result(&self) -> String;
+}
+
+pub fn create_game(game_type: &GameType, payload: &str) -> Result<Box<dyn Game>> {
+    match game_type {
+        GameType::Fts => {
+            match serde_json::from_str(payload) {
+                Ok(config) => Ok(Box::new(Fts::init(config)?)),
+                Err(e) => Err(anyhow!(Error::ParseConfig(e.to_string())))
+            }
+        }
+    }
 }
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,14 +1,40 @@
-use axum::extract::{Path, State};
+use std::collections::HashMap;
+use anyhow::Error;
+use axum::extract::{Path, Query, State};
+use axum::http::StatusCode;
 use axum::Json;
+use axum::response::{IntoResponse, Response};
+use serde::Deserialize;
 use crate::config;
 use crate::game;
 use crate::app_state::AppState;
 
-pub(crate) async fn create_game(State(state): State<AppState>,
-                                Json(config) : Json<config::Fts>) -> String {
+#[derive(Deserialize)]
+pub struct GameTypeQuery {
+    kind: game::GameType
+}
 
+pub struct AnyhowError(Error);
+
+impl IntoResponse for AnyhowError {
+    fn into_response(self) -> Response {
+        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+    }
+}
+
+impl<E> From<E> for AnyhowError where E: Into<Error>{
+    fn from(value: E) -> Self {
+        Self(value.into())
+    }
+}
+
+pub(crate) async fn create_game(State(state): State<AppState>,
+                                Query(typeParam) : Query<GameTypeQuery>,
+                                body: String) -> Result<String, AnyhowError> {
+
+    let game = game::create_game(&typeParam.kind, &body)?;
     let mut storage = state.game_store.lock().unwrap();
-    storage.insert_game(Box::new(game::Fts::init(config).unwrap()))
+    Ok(storage.insert_game(game))
 }
 
 pub(crate) async fn start_game(Path(id): Path<String>,
