b0718a63ce994e0eaa72f0352e2f5c23115e7a99
zixuansong <zsong249@gmail.com>
2024-02-26 01:01:50 -0800

    move build to after install

--- aws/build.sh
+++ aws/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 export PATH="$HOME/.cargo/bin:$PATH"
+
+cd /home/ec2-user/app/source
 cargo build --release
+
+mv ./target/release/ /home/ec2-user/app/bin
+rm -rf /home/ec2-user/app/source
+
--- aws/start-app.sh
+++ aws/start-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #pipe to dev null for now, need to hook up tp cloud watch
-nohup /home/ec2-user/zsonkorp > /dev/null 2>&1 &
+nohup /home/ec2-user/bin/zsonkorp > /dev/null 2>&1 &
